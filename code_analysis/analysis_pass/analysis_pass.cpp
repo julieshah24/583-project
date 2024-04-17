f10fab681e91889d7ce3b4cddcb81a5d634b6f88
Daphne Victoria Tsai <dvtsai@eecs583a.eecs.umich.edu>
2024-04-16 21:24:03 -0400

    Loop profiling data

--- code_analysis/analysis_pass/analysis_pass.cpp
+++ code_analysis/analysis_pass/analysis_pass.cpp
@@ -14,93 +14,68 @@ namespace {
 struct AnalysisPass : public PassInfoMixin<AnalysisPass> {
 
   PreservedAnalyses run(Function &F, FunctionAnalysisManager &FAM) {
-    // These variables contain the results of some analysis passes.
-    llvm::BlockFrequencyAnalysis::Result &bfi = FAM.getResult<BlockFrequencyAnalysis>(F); // BlockFrequencyInfo
-    llvm::BranchProbabilityAnalysis::Result &bpi = FAM.getResult<BranchProbabilityAnalysis>(F);
+    auto &bfi = FAM.getResult<BlockFrequencyAnalysis>(F);
+    auto &LI = FAM.getResult<LoopAnalysis>(F);
+    auto &SE = FAM.getResult<ScalarEvolutionAnalysis>(F);
+    //errs() << "Hello!";
 
-    double intALU =     0; // integer alu operations
-    double fpALU =      0; // floating point alu operations
-    double memory =     0; // memory operations
-    double others =     0; // other operations
-    double biased =     0; // biased branch operations
-    double unbiased =   0; // unbiased branch operations
-    double dynOpCount = 0; // dynamic operation count
+    for (Function::iterator func_iter = F.begin(); func_iter != F.end(); func_iter++){
 
-    // Iterate through basic blocks in function
-    for (Function::iterator func_iter = F.begin(); func_iter != F.end(); func_iter++) {
-      BasicBlock *curr_block = &(*func_iter); // pointer to current block
+      // numbering basic blocks, not really helpful
+      // std::unordered_map<BasicBlock*, unsigned> blockIDs;
+      // unsigned id = 0;
+      // for (BasicBlock &BB : F) {
+      //     blockIDs[&BB] = id++;
+      // }
+      //errs() << "going through function";
 
-      // Number of times block runs
-      std::optional<long unsigned int> init_count = bfi.getBlockProfileCount(curr_block);
-      double count = init_count.value_or(0);
+        // use &li to iterate through the loops
+        // this way, we don't have to iterate through each basic block and figure out which one has loop
+        // loop through all of the loops
+        for (LoopInfo::iterator loop_iter = LI.begin(); loop_iter != LI.end(); loop_iter++){
+            //errs() << "going through loops";
+            BasicBlock *Header = (*loop_iter)->getHeader();
+            // errs() << "This is the loop " << "\n";
+            // errs() << Header << "\n";
+            // unsigned blockID = blockIDs[Header];
+            uint64_t HeaderFreq = bfi.getBlockFreq(Header).getFrequency();
 
-      // Iterate through instructions in current basic block
-      for (BasicBlock::iterator bb_iter = func_iter->begin(); bb_iter != func_iter->end(); bb_iter++) {
-        unsigned opcode = bb_iter->getOpcode(); // instruction opcode
-        dynOpCount += count; // dynamic number of times instruction runs
+            uint64_t CyclesPerIteration = 10;
+            uint64_t TotCycles = HeaderFreq * CyclesPerIteration;
 
-        // Branch - br, switch, indirectbr
-        if(opcode == Instruction::Br || 
-           opcode == Instruction::Switch || 
-           opcode == Instruction::IndirectBr) {
-          
-          bool is_biased = false; // initial value
-          
-          // For each successor of current block
-          for (BasicBlock *succesor: successors(curr_block)) {
-            // Get probability from current block to successor
-            BranchProbability edge_prob = bpi.getEdgeProbability(curr_block, succesor);
-            if (edge_prob > BranchProbability(80, 100)) {
-              is_biased = true;
-              break; // found a biased branch
-            }
+            // errs() << "Loop in function " << F.getName() << " with header block " << Header->getName()
+            //        << " executes approximately " << HeaderFreq << " times.\n"
+            //        << " Estimated total cycles (assuming " << CyclesPerIteration << " cycles/iteration): " << TotCycles << "\n";
+            errs() << "LoopID: " << Header << " ";
+            //errs() << "LoopID: " << (*loop_iter)->getHeader()->getName() << " ";
+            //errs() << "Location: " << File.str() << ":" << LineNo << " ";
+            errs() << "Executions: " << HeaderFreq << " ";
+            errs() << "Cycles: " << TotCycles << " ";
+            errs() << "Cycles per Execution: " << CyclesPerIteration << "\n";
+            // Get debug location information
+            //if (const DebugLoc &DL = Header->getTerminator()->getDebugLoc()) {
+              // errs() << "going through loop info";
+              //unsigned LineNo = DL.getLine();
+              //StringRef File = DL->getFilename();
+
+            //   // Calculate loop trip count and total execution count
+            //   std::optional<unsigned> TripCount = SE.getSmallConstantTripCount(*loop_iter);
+            //   errs() << TripCount;
+            //   std::optional<long unsigned int> BlockCount = bfi.getBlockProfileCount(Header);
+            //   double TotalExecutions = TripCount.has_value() && BlockCount ? TripCount.value() * BlockCount.value() : 0;
+
+            //   // Estimate the total clock cycles (simple estimation: 10 cycles per iteration for now)
+            //   double TotalCycles = TotalExecutions * 10;  // Simplified assumption
+              
+
+            //   // Print loop information
+            //   errs() << "LoopID: " << (*loop_iter)->getHeader()->getName() << " ";
+            //   //errs() << "Location: " << File.str() << ":" << LineNo << " ";
+            //   errs() << "Executions: " << TotalExecutions << " ";
+            //   errs() << "Cycles: " << TotalCycles << "\n";
+            // //}
           }
-
-          // Increment appropriate count
-          if(is_biased) biased += count;
-          else          unbiased += count;
-        }
-
-        // Integer alu - add, sub, mul, udiv, sdiv, urem, shl, lshr, ashr, and, or, xor, icmp, srem
-        else if(opcode == Instruction::Add || opcode == Instruction::UDiv ||
-                opcode == Instruction::Sub || opcode == Instruction::SDiv ||
-                opcode == Instruction::Mul || opcode == Instruction::URem ||
-                opcode == Instruction::Shl || opcode == Instruction::LShr || 
-                opcode == Instruction::And || opcode == Instruction::ICmp ||
-                opcode == Instruction::Xor || opcode == Instruction::AShr || 
-                opcode == Instruction::Or  || opcode == Instruction::SRem) {
-          intALU += count;
-        }
-
-        // Floating point alu - fadd, fsub, fmul, fdiv, frem, fcmp
-        else if(opcode == Instruction::FAdd || opcode == Instruction::FSub || 
-                opcode == Instruction::FMul || opcode == Instruction::FDiv || 
-                opcode == Instruction::FRem || opcode == Instruction::FCmp) {
-          fpALU += count;
-        }
-
-        // Memory - alloca, load, store, getelementptr, fence, atomiccmpxchg, atomicrmw
-        else if(opcode == Instruction::Store || opcode == Instruction::GetElementPtr ||
-                opcode == Instruction::Fence || opcode == Instruction::AtomicCmpXchg ||
-                opcode == Instruction::Load  || opcode == Instruction::Alloca ||
-                opcode == Instruction::AtomicRMW) {
-          memory += count;
-        }
-
-        // Others
-        else others += count;
-      }
     }
-
-    // Print data
-    errs() << F.getName();
-    errs() << ", " << static_cast<uint64_t>(dynOpCount);
-    // errs() << ", " << dynOpCount;
-    for (double instr : {intALU, fpALU, memory, biased, unbiased, others}) { // iterate all categories
-      if (dynOpCount == 0) errs() << ", " << format("%.3f", 0.000);
-      else errs() << ", " << format("%.3f", instr / dynOpCount);
-    }
-    errs() << "\n";
 
     return PreservedAnalyses::all();
   }
