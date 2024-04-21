642f442f252f0b1a46b2532e3fdb39230ce743cd
leah99mac <leah.m.mackay@gmail.com>
2024-04-20 20:54:19 -0400

    query.py is a file that queries LLAMA 3

---
+++ query.py
@@ -0,0 +1,92 @@
+###################################################################
+#                                                                 #
+# This script takes a prompt file and uses its contents to prompt #
+# the LLM LLAMA 3. The output produced by LLAMA 3 will then be    #
+# saved in the folder "model/outputs/".                           #
+#                                                                 #
+# To use follow the following input:                              #
+#     python query.py <file containing prompt>                    #
+#                                                                 #
+###################################################################
+import replicate
+import sys
+from datetime import datetime
+
+# reads prompt file given in arguments
+def get_prompt(file_name):
+    
+    file_path = 'model/prompts/' + file_name
+    
+    try:
+        file = open(file_path, 'r')
+    except:
+        print('\nERROR WITH PROMPT FILE')
+        print('This script cannot find the prompt file "' + file_name + '" in "model/prompts/"')
+        print('Please make sure the prompt file is located in "model/prompts/"\n')
+        return ''
+
+    try:
+        prompt = file.read()
+    except:
+        print('\nERROR READING PROMPT FILE\n')
+        return ''
+    
+    try:
+        file.close()
+    except:
+        print('\nERROR CLOSING PROMPT FILE\n')
+        return ''
+    
+    return prompt
+
+# queries llama with the given prompt
+def query_llama(prompt):
+    
+    input = {'prompt': prompt, 'prompt_template': '<|begin_of_text|><|start_header_id|>system<|end_header_id|>\n\nYou are a helpful assistant<|eot_id|><|start_header_id|>user<|end_header_id|>\n\n{prompt}<|eot_id|><|start_header_id|>assistant<|end_header_id|>\n\n'}
+   
+    output = replicate.run('meta/meta-llama-3-70b-instruct',
+            input=input)
+
+    print("".join(output))
+    return output
+
+# saves model output to a text file
+def save_model_output(output):
+    
+    now = datetime.now()
+    dt_str = now.strftime('%d%m%Y_%H%M%S')
+    file_path = 'model/output/output_' + dt_str + '.txt'
+    
+    try:
+        file = open(file_path, 'w')
+    except:
+        print('\nERROR OPENING OUTPUT FILE\n')
+        return
+
+    try:
+        file.write(output)
+    except:
+        print('\nERROR WRITING TO OUTPUT FILE\n')
+        return ''
+
+    try:
+        file.close()
+    except:
+        print('\nERROR CLOSING OUTPUT FILE\n')
+        return
+    
+    print('Model output saved to "' + file_path + '"')
+    return
+
+# runs script
+if len(sys.argv) < 2:
+    print('\nERROR WITH INPUT')
+    print('To run this script, please use the following input structure:')
+    print('    python query.py <file containing prompt>\n')
+else:
+    prompt_str = get_prompt(sys.argv[1])
+    if prompt_str != '':
+        output = query_llama(prompt_str)
+        save_model_output("".join(output))
+    else:
+        print('Prompt was empty. LLAMA was not queried.\n')
