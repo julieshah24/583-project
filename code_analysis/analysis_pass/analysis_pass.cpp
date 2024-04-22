a61cea84cdd59727adf5b2496719880db05e931c
Sam Jaehnig <sammyjaehnig@gmail.com>
2024-04-22 18:41:46 -0400

    adding more natural language to outputs

--- code_analysis/analysis_pass/analysis_pass.cpp
+++ code_analysis/analysis_pass/analysis_pass.cpp
@@ -157,25 +157,26 @@ struct AnalysisPass : public PassInfoMixin<AnalysisPass> {
     }
     else {
       //prints func_name: func_frequency, 
-      outFile << funcName << ": ";
-      outFile << functionFrequency << ", ";
-      outFile << numBBs << ", ";
+      outFile << "The function named \"" << funcName << "\" executes ";
+      outFile << functionFrequency << " times. It has ";
+      outFile << numBBs << " basic blocks, where on average each basic block executes ";
       double avgBBFrequency = (double)(totalBBFrequency) / (double)(numBBs);
-      outFile << avgBBFrequency << ", ";
+      outFile << avgBBFrequency << " times, has ";
       double avgBBMemAccess = (double)(totalMemAccess) / (double)(numBBs);
-      outFile << avgBBMemAccess << ", ";
+      outFile << avgBBMemAccess << " memory acceses, ";
       double avgBBBiasedBranches = (double)(totalBiasedBranches) / (double)(numBBs);
-      outFile << avgBBBiasedBranches << ", ";
+      outFile << avgBBBiasedBranches << " biased branches, and ";
       double avgBBUnbiasedBranches = (double)(totalUnbiasedBranches) / (double)(numBBs);
-      outFile << avgBBUnbiasedBranches << ", ";
-      outFile << numLoops << ", ";
+      outFile << avgBBUnbiasedBranches << " unbiased branches. It has ";
+      outFile << numLoops << " loops, where on average each loop executes ";
       double avgExecutionsPerLoop = (double)(totalLoopExecutions) / (double)(numLoops);
-      outFile << avgExecutionsPerLoop << ", ";
+      outFile << avgExecutionsPerLoop << " times, has ";
       double avgCyclesPerLoop = (double)(totalLoopCycles) / (double)(numLoops);
-      outFile << avgCyclesPerLoop << ", ";
+      outFile << avgCyclesPerLoop << " total cycles, and ";
       double avgCyclesPerLoopIteration = (double)(totalCyclesPerIteration) / (double)(numLoops);
-      outFile << avgCyclesPerLoopIteration << "\n";
+      outFile << avgCyclesPerLoopIteration << " cycles per loop iteration.\n";
 
+      outFile << "Based on the profile information presented for the .c file, name relevant llvm optimization compiler flags and nothing else.";
       // outFile << "Name exactly one llvm optimization compile flag and nothing else";
 
       // Close the file
